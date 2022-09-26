f602533e4f509bf2b3572c5fc5dd2a6c38531db5
darkness6830 <99259234+darkness6830@users.noreply.github.com>
2022-09-26 09:38:55 +0300

    Add files via upload

---
+++ tipove danni 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tipove_danni_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char bukva = 'C';
+            sbyte number = 100;
+            short number1 = 20000;
+            int number3 = 300000;
+            string prog = "programirane";
+            Console.WriteLine("promenlivata ot char" + bukva);
+            Console.WriteLine("promenlivata ot sbyte" + number);
+            Console.WriteLine("promenlivata ot short" + number1);
+            Console.WriteLine("promenlivata ot int" + number3);
+            Console.WriteLine("promenlivata ot string" + prog);
+        }
+    }
+}
